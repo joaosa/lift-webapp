0f53451bdcdb82f7b0a12753e8deebbc0d8d0b1c
Joao Andrade <jjadonotenter@gmail.com>
2012-05-22 16:56:35 +0100

    Typeclass naming changes + cleanup

--- src/main/scala/code/service/CRUDifiable.scala
+++ src/main/scala/code/service/CRUDifiable.scala
@@ -4,39 +4,6 @@ import net.liftweb.common.{Empty, Box, Full}
 import code.helper._
 import net.liftweb.util.FieldError
 import net.liftweb.mapper.{BaseMapper, Mapper, KeyedMapper, KeyedMetaMapper}
-import net.liftweb.json._
-import xml.Elem
-
-trait Extractable[T] {
-  def extractField(t: T, field: String): Box[String]
-  def extractModel(t: T, modelName: String): List[T]
-}
-
-object Extractor {
-
-  def extractField[T: Extractable](t: T, fieldName: String) = implicitly[Extractable[T]].extractField(t, fieldName)
-  def extractModel[T: Extractable](t: T, modelName: String) = implicitly[Extractable[T]].extractModel(t, modelName)
-
-  implicit object Json extends Extractable[JValue] {
-    def extractField(t: JValue, field: String): Box[String] = {
-      Box(for (JString(s) <- t \\ field) yield s)
-    }
-    def extractModel(t: JValue, modelName: String): List[JValue] = {
-      (for (JArray(m) <- t \\ modelName) yield m).flatten
-    }
-  }
-
-  implicit object Xml extends Extractable[Elem] {
-    def extractField(t: Elem, field: String): Box[String] = {
-      Full((t \\ field).text)
-    }
-    def extractModel(t: Elem, modelName: String): List[Elem] = {
-      (t \\ modelName).seq.toList.map(_.asInstanceOf[Elem])
-    }
-  }
-
-}
-
 
 trait CRUDifiable[CRUDType <: KeyedMapper[_, CRUDType]] {
   self: CRUDType with KeyedMetaMapper[_, CRUDType] =>
