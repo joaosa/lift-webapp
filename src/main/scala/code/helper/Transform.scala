855d3036edd78254a5e846f7ba854899367230d2
Joao Andrade <jjadonotenter@gmail.com>
2012-05-28 16:41:58 +0100

    code formatting

--- src/main/scala/code/helper/Transform.scala
+++ src/main/scala/code/helper/Transform.scala
@@ -11,34 +11,40 @@ import java.util.Date
 sealed class Transform(p: Any) {
   def apply(b: Box[String]): Box[Any] = Full(p)
 }
+
 object Transform
 
 object Identity extends Transform {
   override def apply(b: Box[String]): Box[String] = b
 }
+
 object ToInt extends Transform {
   override def apply(b: Box[String]): Box[Int] = {
     for (v <- b) yield v.toInt
   }
 }
+
 object ToLong extends Transform {
   override def apply(b: Box[String]): Box[Long] = {
     for (v <- b) yield v.toLong
   }
 }
+
 object ToDouble extends Transform {
   override def apply(b: Box[String]): Box[Double] = {
     for (v <- b) yield v.toDouble
   }
 }
+
 object ByUserName extends Transform {
   override def apply(v: Box[String]): Box[Long] = {
-      User.find(Like(User.username, v openOr "")) match {
+    User.find(Like(User.username, v openOr "")) match {
       case Full(f) => Full(f.id.is)
       case _ => Empty
     }
   }
 }
+
 object ByEmail extends Transform {
   override def apply(v: Box[String]): Box[Long] = {
     User.find(Like(User.email, v openOr "")) match {
@@ -47,11 +53,13 @@ object ByEmail extends Transform {
     }
   }
 }
+
 object Now extends Transform {
   override def apply(b: Box[String]): Box[Date] = {
     Full(now)
   }
 }
+
 object StaticString {
   def apply(s: String): Transform = {
     new Transform(s)
